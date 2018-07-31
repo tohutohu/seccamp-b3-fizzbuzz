27af913de63abe56c52d5323c3b3618c1c8b618c
とーふとふ <tohu.soy@gmail.com>
2018-08-01 02:26:54 +0900

    add ping

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/ping", pingpong)
+
+	e.Start(":8080")
+}
+
+func pingpong(c echo.Context) error {
+	return c.String(http.StatusOK, "pong")
+}
